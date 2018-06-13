243cac37bb3007629f3e9113da5ceec6c15347e9
sun12002 <sufian.nazim@uconn.edu>
2018-06-13 14:42:24 -0600

    refactored

--- src/main/java/io/pivotal/pal/tracker/controller/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/controller/TimeEntryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/time-entries")
+@RestController
 public class TimeEntryController {
 
     private TimeEntryRepository timeEntryRepository;
@@ -19,7 +19,7 @@ public class TimeEntryController {
         this.timeEntryRepository = timeEntryRepository;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/time-entries/{id}")
     public ResponseEntity<TimeEntry> read(@PathVariable long id){
         TimeEntry timeEntry = timeEntryRepository.find(id);
         if (timeEntry == null) {
@@ -28,17 +28,17 @@ public class TimeEntryController {
         return new ResponseEntity<TimeEntry>(timeEntry, HttpStatus.OK);
     }
 
-    @PostMapping
+    @PostMapping("/time-entries")
     public ResponseEntity create(@RequestBody TimeEntry timeEntry){
         return new ResponseEntity<TimeEntry>(timeEntryRepository.create(timeEntry), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping("/time-entries")
     public ResponseEntity<List<TimeEntry>> list() {
         return new ResponseEntity<List<TimeEntry>>(timeEntryRepository.list(), HttpStatus.OK);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/time-entries/{id}")
     public ResponseEntity update(@PathVariable long id, @RequestBody TimeEntry timeEntry){
         TimeEntry entry = timeEntryRepository.update(id, timeEntry);
         if (entry == null) {
@@ -47,7 +47,7 @@ public class TimeEntryController {
         return new ResponseEntity<TimeEntry>(entry, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/time-entries/{id}")
     public ResponseEntity<TimeEntry> delete(@PathVariable long id) {
         timeEntryRepository.delete(id);
         return new ResponseEntity<TimeEntry>(HttpStatus.NO_CONTENT);
--- src/main/java/io/pivotal/pal/tracker/repository/TimeEntry.java
+++ src/main/java/io/pivotal/pal/tracker/repository/TimeEntry.java
@@ -17,7 +17,6 @@ public class TimeEntry {
     public TimeEntry(long projectId, long userId, LocalDate date, int hours){
         this.projectId = projectId;
         this.userId = userId;
-
         this.date = date;
         this.hours = hours;
     }
